30fda423c8b07346c2769a9362f12b13bf551484
José Bernal Fonseca <macmini@Mac-mini.lan>
2020-09-18 12:35:58 -0500

    Codigo SQL para crear una base de datos en MSSQL-SERVER para hacer pruebas y codigo para conectar a dicha base de datos desde c#, se incluye captura de conexion exitosa

---
+++ Desarrollo_de_apps/Practica4/EjemploConexionSQL/EjemploConexionSQL/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using System.Data.SqlClient;  //librería para el uso de sql-server
+
+namespace EjemploConexion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string cadConect = @"Server=localhost;User=sa;Password=Repstalker1;Database=rescate_imposible;";
+            SqlConnection mi_conexion = new SqlConnection(cadConect);
+
+            try
+            {
+                mi_conexion.Open(); //Abre la conexion a la base de datos "Escuelita"
+                Console.WriteLine("Me conecte a la base!!!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                mi_conexion.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
